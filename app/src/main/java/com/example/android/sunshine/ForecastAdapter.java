b40ffa427701e447652645196d4c8695dedb1675
Tom Lyons <tbl3rd@gmail.com>
2014-09-10 22:29:46 -0400

    Compilation checkpoint after adding gists supplied by tutorial.

--- app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
+++ app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -23,49 +23,22 @@ public class ForecastAdapter extends CursorAdapter
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-
-        // Read weather icon ID from cursor
-        final int weatherId = cursor.getInt(ForecastFragment.COL_WEATHER_ID);
-
-        // Use placeholder image for now
-        final ImageView iconView
-            = (ImageView)view.findViewById(R.id.list_item_icon);
-        iconView.setImageResource(R.drawable.ic_launcher);
-
-        // Read date from cursor
-        final String dateString
-            = cursor.getString(ForecastFragment.COL_WEATHER_DATE);
-
-        // Find TextView and set formatted date on it
-        final TextView dateView
-            = (TextView)view.findViewById(R.id.list_item_date_textview);
-        dateView.setText(Utility.getFriendlyDayString(context, dateString));
-
-        // Read weather forecast from cursor
-        final String description
-            = cursor.getString(ForecastFragment.COL_WEATHER_DESC);
-        // Find TextView and set weather forecast on it
-        final TextView descriptionView
-            = (TextView)view.findViewById(R.id.list_item_forecast_textview);
-        descriptionView.setText(description);
-
-        // Read user preference for metric or imperial temperature units
-        final boolean isMetric = Utility.isMetric(context);
-
-        // Read high temperature from cursor
-        final float high
-            = cursor.getFloat(ForecastFragment.COL_WEATHER_MAX_TEMP);
-
-        // TODO: Find TextView and set formatted high temperature on it
-
-        // Read low temperature from cursor
-        final float low
-            = cursor.getFloat(ForecastFragment.COL_WEATHER_MIN_TEMP);
-
-        // TODO: Find TextView and set formatted low temperature on it
+        ((ImageView)view.findViewById(R.id.list_item_weather_icon))
+            .setImageResource(R.drawable.ic_launcher);
+        ((TextView)view.findViewById(R.id.list_item_date))
+            .setText(Utility.getFriendlyDayString(context,
+                            cursor.getString(Utility.COLUMN_DATE)));
+        ((TextView)view.findViewById(R.id.list_item_description))
+            .setText(cursor.getString(Utility.COLUMN_DESCRIPTION));
+        ((TextView)view.findViewById(R.id.list_item_maximum))
+            .setText(Utility.fromCelsius(context,
+                            cursor.getDouble(Utility.COLUMN_MAXIMUM)));
+        ((TextView)view.findViewById(R.id.list_item_minimum))
+            .setText(Utility.fromCelsius(context,
+                            cursor.getDouble(Utility.COLUMN_MINIMUM)));
     }
 
-    public ForecastAdapter(Context context, Cursor c, int flags) {
-        super(context, c, flags);
+    public ForecastAdapter(Context context, Cursor cursor, int flags) {
+        super(context, cursor, flags);
     }
 }
--- app/src/main/java/com/example/android/sunshine/Utility.java
+++ app/src/main/java/com/example/android/sunshine/Utility.java
@@ -90,57 +90,41 @@ public class Utility
         Toast.makeText(c, c.getString(stringResId), Toast.LENGTH_SHORT).show();
     }
 
-    // Format used for storing dates in the database.  ALso used for
-    // converting those strings back into date objects for
-    // comparison/processing.
-    public static final String DATE_FORMAT = "yyyyMMdd";
-
     /**
      * Helper method to convert the database representation of the
      * date into something to display to users.  As classy and
      * polished a user experience as "20140102" is, we can do better.
      *
      * @param context Context to use for resource localization
-     * @param dateStr The db formatted date string, expected to be of
-     *                the form specified in Utility.DATE_FORMAT
+     * @param dbDate The db formatted date string
      * @return a user-friendly representation of the date.
      */
-    public static String getFriendlyDayString(Context context, String dateStr) {
-        // The day string for forecast uses the following logic:
-        // For today: "Today, June 8"
-        // For tomorrow:  "Tomorrow"
-        // For the next 5 days: "Wednesday" (just the day name)
-        // For all days after that: "Mon Jun 8"
-
-        final Date todayDate = new Date();
-        final String todayStr = WeatherContract.getDbDateString(todayDate);
-        final Date inputDate = WeatherContract.getDateFromDb(dateStr);
-
-        // If the date we're building the String for is today's date, the format
-        // is "Today, June 24"
-        if (todayStr.equals(dateStr)) {
-            final String today = context.getString(R.string.today);
+    //
+    // The day string for forecast uses the following logic:
+    // For today: "Today, June 8"
+    // For tomorrow:  "Tomorrow"
+    // For the next 5 days: "Wednesday" (just the day name)
+    // For all days after that: "Mon Jun 8"
+    //
+    public static String getFriendlyDayString(Context context, String dbDate) {
+        final Date today = new Date();
+        if (WeatherEntry.dbDate(today).equals(dbDate)) {
             return context.getString(
                     R.string.format_full_friendly_date,
-                    today,
-                    getFormattedMonthDay(context, dateStr));
+                    context.getString(R.string.today),
+                    getFormattedMonthDay(context, dbDate));
+        }
+        final Calendar cal = Calendar.getInstance();
+        cal.setTime(today);
+        cal.add(Calendar.DATE, 7);
+        if (dbDate.compareTo(WeatherEntry.dbDate(cal.getTime())) < 0) {
+            // If the input date is less than a week in the
+            // future, just return the day name.
+            return getDayName(context, dbDate);
         } else {
-            final Calendar cal = Calendar.getInstance();
-            cal.setTime(todayDate);
-            cal.add(Calendar.DATE, 7);
-            final String weekFutureString
-                = WeatherContract.getDbDateString(cal.getTime());
-
-            if (dateStr.compareTo(weekFutureString) < 0) {
-                // If the input date is less than a week in the
-                // future, just return the day name.
-                return getDayName(context, dateStr);
-            } else {
-                // Otherwise, use the form "Mon Jun 3"
-                final SimpleDateFormat shortenedDateFormat
-                    = new SimpleDateFormat("EEE MMM dd");
-                return shortenedDateFormat.format(inputDate);
-            }
+            // Otherwise, use the form "Mon Jun 3"
+            return new SimpleDateFormat("EEE MMM dd")
+                .format(WeatherEntry.dbDate(dbDate));
         }
     }
 
@@ -149,74 +133,36 @@ public class Utility
      * E.g "today", "tomorrow", "wednesday".
      *
      * @param context Context to use for resource localization
-     * @param dateStr The db formatted date string, expected to be of
-     *                the form specified in Utility.DATE_FORMAT
+     * @param dbDate The db formatted date string
      * @return
      */
-    public static String getDayName(Context context, String dateStr) {
-        final SimpleDateFormat dbDateFormat
-            = new SimpleDateFormat(Utility.DATE_FORMAT);
-        try {
-            final Date inputDate = dbDateFormat.parse(dateStr);
-            final Date todayDate = new Date();
-            // If the date is today, return the localized version of
-            // "Today" instead of the actual day name.
-            if (WeatherContract.getDbDateString(todayDate).equals(dateStr)) {
-                return context.getString(R.string.today);
-            } else {
-                // If the date is set for tomorrow, the format is "Tomorrow".
-                final Calendar cal = Calendar.getInstance();
-                cal.setTime(todayDate);
-                cal.add(Calendar.DATE, 1);
-                final Date tomorrowDate = cal.getTime();
-                if (WeatherContract.getDbDateString(tomorrowDate).equals(
-                                dateStr)) {
-                    return context.getString(R.string.tomorrow);
-                } else {
-                    // Otherwise, the format is just the day of the
-                    // week (e.g "Wednesday".
-                    return new SimpleDateFormat("EEEE").format(inputDate);
-                }
-            }
-        } catch (ParseException e) {
-            e.printStackTrace();
-            // It couldn't process the date correctly.
-            return "";
+    public static String getDayName(Context context, String dbDate) {
+        final Date inputDate = WeatherEntry.dbDate(dbDate);
+        final Date today = new Date();
+        // If the date is today, return the localized version of
+        // "Today" instead of the actual day name.
+        if (WeatherEntry.dbDate(today).equals(dbDate)) {
+            return context.getString(R.string.today);
         }
+        // If the date is set for tomorrow, the format is "Tomorrow".
+        final Calendar calendar = Calendar.getInstance();
+        calendar.setTime(today);
+        calendar.add(Calendar.DATE, 1);
+        return (WeatherEntry.dbDate(calendar.getTime()).equals(dbDate))
+            ? context.getString(R.string.tomorrow)
+            : new SimpleDateFormat("EEEE").format(inputDate);
     }
 
     /**
      * Converts db date format to the format "Month day", e.g "June 24".
      *
      * @param context Context to use for resource localization
-     * @param dateStr The db formatted date string, expected to be of
-     *                the form specified in Utility.DATE_FORMAT
+     * @param dbDate The db formatted date string
      * @return The day in the form of a string formatted "December 6"
      */
-    public static String getFormattedMonthDay(Context context, String dateStr)
+    public static String getFormattedMonthDay(Context context, String dbDate)
     {
-        final SimpleDateFormat dbDateFormat
-            = new SimpleDateFormat(Utility.DATE_FORMAT);
-        try {
-            final Date inputDate = dbDateFormat.parse(dateStr);
-            final SimpleDateFormat monthDayFormat
-                = new SimpleDateFormat("MMMM dd");
-            final String monthDayString = monthDayFormat.format(inputDate);
-            return monthDayString;
-        } catch (ParseException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    /**
-     * Returns true if metric unit should be used, or false if
-     * imperial units should be used.
-     */
-    public static boolean isMetric(Context context) {
-        return PreferenceManager.getDefaultSharedPreferences(context)
-            .getString(context.getString(R.string.pref_units_key),
-                    context.getString(R.string.pref_units_metric)).equals(
-                            context.getString(R.string.pref_units_metric));
+        return new SimpleDateFormat("MMMM dd")
+            .format(WeatherEntry.dbDate(dbDate));
     }
 }
